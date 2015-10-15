f941e8724c594a5830b59a95134b32f90b7f1d71
Ben Beck <ben.beck.764@gmail.com>
2015-10-15 11:33:12 -0500

    Moved node.js files into "web-server" directory. Added a node.js route for testing, and enabled CORS for node.js.

--- web-server/app.js
+++ web-server/app.js
@@ -3,6 +3,17 @@ var app = express();
 var bodyParser = require('body-parser');
 app.use(bodyParser());
 
+//CORS middleware
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+
+    next();
+}
+
+app.use(allowCrossDomain);
+
 
 app.get('/', function (req, res) {
   // res.send('Hello World!');
@@ -11,8 +22,12 @@ app.get('/', function (req, res) {
 
 app.post('/', function(req, res)
 {
+	
   console.log(req.body.hi);
-  
+});
+
+app.get('/test', function (req, res) {
+	res.send("test");
 });
 
 var server = app.listen(3000, function () {
