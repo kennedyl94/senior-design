9ba6b2da8e5968423872d6b7219052d74a042619
Ben Beck <ben.beck.764@gmail.com>
2016-01-04 20:06:33 -0600

    Updated Angular unit tests in order to properly use the 'config' module.

--- angular-client/app/config/config.module.js
+++ angular-client/app/config/config.module.js
@@ -1,7 +1,7 @@
 (function() {
   'use strict';
 
-  angular.module('config', [])
+  angular.module('config', ['ui.router'])
     .value('config', {
       //domain: 'http://localhost:3000/api/'
       domain: 'http://orgmatcher.msoe.edu/api/'
