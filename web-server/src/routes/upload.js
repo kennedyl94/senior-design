8b5861b8cd392e081f7a45917d51f66f57d82235
Paul Hobson <hobsonpa@msoe.edu>
2016-01-07 23:26:16 -0600

    Modifed upload route to use a multi-part parser to help clean it up.

--- web-server/src/routes/upload.js
+++ web-server/src/routes/upload.js
@@ -1,6 +1,7 @@
 var express = require('express')
   , router = express.Router();
-  
+
+var multer = require('multer');
 var csvParse = require('csv-parse');
 
 var _dataServices = require('../orgDataServices');
@@ -9,52 +10,48 @@ var fs = require('fs');
 
 var body = '';
 
-router.post('/', function(req, res){    
-    req.on('data', function(chunk){
-        body += chunk;
-    });
-    req.on('end', function(){
-        var input = body.substring(body.indexOf('text/csv') + 12, body.lastIndexOf('-----------------------------') -2); 
-        csvParse(input, function(err, output){
-            
-            var orgNameIndex = -1;
-            var descriptionIndex = -1;
-            var contactFirstNameIndex = -1;
-            var contactLastNameIndex = -1;
-            var emailIndex = -1;
-            var phoneIndex = -1;
-            
-            for (rowIndex in output){
-                var row = output[rowIndex];
-                if(orgNameIndex < 0){
-                    orgNameIndex = row.indexOf('Organization Name');
-                    descriptionIndex = row.indexOf('Organization Description');
-                    contactFirstNameIndex = row.indexOf('President\'s Name (First)');
-                    contactLastNameIndex = row.indexOf('President\'s Name (Last)');
-                    emailIndex = row.indexOf('President\'s MSOE Email');
-                    phoneIndex = row.indexOf('President\'s Phone Number');
-                }
-                else{
-                    var newOrg = {
-                        name: row[orgNameIndex],
-                        description: row[descriptionIndex],
-                        tags: [],
-                        active: true,
-                        contact: {
-                            name: row[contactFirstNameIndex] + ' ' + row[contactLastNameIndex],
-                            email: row[emailIndex],
-                            phone: row[phoneIndex]
-                        }
-                    }
-                    
-                    console.log(newOrg);
-                    console.log('\n\n');
-                    _dataServices.addStudentOrg(newOrg, function(err, returnedOrg){
-                        console.log(err);
-                    });
-                }
-            }
-        });
+var uploader = multer({storage: multer.memoryStorage()});
+
+router.post('/', uploader.single('file'), function(req, res){
+	console.log(req.file.buffer.toString());    
+	csvParse(req.file.buffer.toString(), function(err, output){
+		
+		var orgNameIndex = -1;
+		var descriptionIndex = -1;
+		var contactFirstNameIndex = -1;
+		var contactLastNameIndex = -1;
+		var emailIndex = -1;
+		var phoneIndex = -1;
+		
+		for (rowIndex in output){
+			var row = output[rowIndex];
+			if(rowIndex == 0){
+				orgNameIndex = row.indexOf('Organization Name');
+				descriptionIndex = row.indexOf('Organization Description');
+				contactFirstNameIndex = row.indexOf('President\'s Name (First)');
+				contactLastNameIndex = row.indexOf('President\'s Name (Last)');
+				emailIndex = row.indexOf('President\'s MSOE Email');
+				phoneIndex = row.indexOf('President\'s Phone Number');
+			}
+			else{
+				var newOrg = {
+					name: row[orgNameIndex],
+					description: row[descriptionIndex],
+					tags: [],
+					active: true,
+					contact: {
+						name: row[contactFirstNameIndex] + ' ' + row[contactLastNameIndex],
+						email: row[emailIndex],
+						phone: row[phoneIndex]
+					}
+				}
+				_dataServices.addStudentOrg(newOrg, function(err, returnedOrg){
+					if(err){
+						res.send(422);
+					}
+				});
+			}
+        };
         res.send(200);
     });
 });
