8a0c56b90b301a5e342d2d9c6e5f872c11d67992
kennedyl <kennedyl@msoe.edu>
2016-01-18 16:09:11 -0600

    fixed issue. it had to do with id being named _id and that messed things up. things are still  not being sent in the post correctly.

--- web-server/src/routes/survey.js
+++ web-server/src/routes/survey.js
@@ -4,33 +4,30 @@ var express = require('express')
   var _surveyData = require('../surveyDataServices.js');
  
 //mock data until database is done 
-var questions = [
-	{question: "do you like pie?",
-	tags:["pie","cool","american"],
-	id:0},
-	{question: "do you like unicycling?",
-	tags:["cool", "outdoors"],
-	id:5},
-	{question: "do you like dogs?",
-	tags:["cool", "dogs"],
-	id:6}
-];
+// var questions = [
+// 	{question: "do you like pie?",
+// 	tags:["pie","cool","american"],
+// 	id:0},
+// 	{question: "do you like unicycling?",
+// 	tags:["cool", "outdoors"],
+// 	id:5},
+// 	{question: "do you like dogs?",
+// 	tags:["cool", "dogs"],
+// 	id:6}
+// ];
 
 router.get('/', function (req, res) {
     // adding questions if there arnt any
     // this should be removed later
-    console.log("test");
+    // console.log("test");
     
-        
-     
-        
     var q;
     _surveyData.getAllQuestions(null, function(questionMap){
         // q = questionMap;
         
         res.send(questionMap);
-        console.log(questionMap);
-        console.log(questionMap.length);
+        // console.log(questionMap);
+        // console.log(questionMap.length);
         
         if(!Object.keys(questionMap).length || questionMap.legnth <= 0)
         {
@@ -53,36 +50,40 @@ router.post("/", function (req, res) {
 	// console.log("wubalubadubdub ");
 	// var x = getOrgsFromAns(req.body)
 	var data = req.body;
-	console.log(data)
-	var yes = getIDFromPost(data)
+	console.log("posted: "+data);
+	var yes = getIDFromPost(data);
     var questions =[];
     var i =0;
     for (i; i< yes.length; i++) {
         
-        var q;
+        
         _surveyData.getQuestionById(yes[i], function(questionMap){
-            q = questionMap;
-            questions.push(q);
+           
+            questions.push(questionMap);
+            if(i == yes.length) {
+                questions = questions.sort();
+            }
         }, function(e){
             console.log(e);
         });
        
     }
-    var tags =[];
-    i =0;
-    for(i; i<questions.length; i++) {
-        var j= 0;
-        for (j; j<questions[i].tags.length; j++) {
-            var k=0;
-            for(k; k< tags.length; k++) {
-                if(tags[k] == questions[i].tags[j]) {
-                    tags.splice(tags[k],1);
-                }
-            }
-            tags.push(questions[i].tags[j]);
-        }
-    }
-    console.log(tags);
+    console.log("questions: "+questions);
+    // var tags =[];
+    // i =0;
+    // for(i; i<questions.length; i++) {
+    //     var j= 0;
+    //     for (j; j<questions[i].tags.length; j++) {
+    //         var k=0;
+    //         for(k; k< tags.length; k++) {
+    //             if(tags[k] == questions[i].tags[j]) {
+    //                 tags.splice(tags[k],1);
+    //             }
+    //         }
+    //         tags.push(questions[i].tags[j]);
+    //     }
+    // }
+    // console.log(tags);
     
 	// console.log(yes);
 // 	data.forEach(function (item) {
