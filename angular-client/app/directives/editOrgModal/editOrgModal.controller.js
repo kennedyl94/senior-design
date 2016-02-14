8a13e217a78f7f888663a48f0bb2899bb722d2dd
Ben Beck <ben.beck.764@gmail.com>
2016-02-14 13:21:56 -0600

    still working on editOrgModal

--- angular-client/app/directives/editOrgModal/editOrgModal.controller.js
+++ angular-client/app/directives/editOrgModal/editOrgModal.controller.js
@@ -6,7 +6,6 @@ function Controller($modalInstance, contents) {
   var vm = this;
 
   vm.org = contents.org;
-  vm.images = contents.images;
 
   vm.back = function() {
     $modalInstance.close('ok');
