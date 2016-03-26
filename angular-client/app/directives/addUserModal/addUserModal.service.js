b22a434cbf55a064a4fce2ff59d14c64adaa9cf7
Elizabeth Donohew <donohewe@MSOE-5CB3270VZG.msoe.edu>
2016-03-25 23:52:05 -0500

    Got the org dropdown menu working for addUserModal

--- angular-client/app/directives/addUserModal/addUserModal.service.js
+++ angular-client/app/directives/addUserModal/addUserModal.service.js
@@ -7,6 +7,46 @@
 
     var service = this;
 
+    service.data = {
+      orgs: {}
+    };
+
+    function init() {
+      var promises = [];
+      promises.push($http({method: 'GET', url: config.domain + 'Organizations/name'}));
+      $q.all(promises).then(function(data) {
+        for(var i = 0; i< data[0].data.length; i++) {
+          service.data.orgs[i] = data[0].data[i].name;
+          console.log(service.data.orgs[i]);
+        }
+      });
+    }
+
+    init();
+
+
+
+    //function init() {
+    //  var deferred = $q.defer();
+    //  var promise = $http({method: 'GET', url: config.domain + 'Organizations/name'});
+    //  promise.then(function(data) {
+    //    console.log("Data: " + data);
+    //    service.orgs.orgs = data[0].data;
+    //    console.log(service.orgs.orgs[1].name);
+    //    console.log("Org Map: " + service.orgMap.orgs);
+    //    for(var i = 0; i < service.orgs.orgs.length; i++) {
+    //      service.orgMap.orgs[i] = {key:service.orgs.orgs[i].name, value: service.orgs.orgs[i].name}
+    //      console.log("Key: " + service.orgMap.orgs[i].key);
+    //      console.log("Value: " + service.orgMap.orgs[i].value);
+    //    }
+    //    console.log(service.orgMap.orgs[1].key);
+    //    deferred.resolve(service.orgMap.orgs);
+    //  });
+    //  return deferred.promise();
+    //}
+
+    init();
+
     service.saveNewUser = function(user) {
       var deferred = $q.defer();
       var promise = $http({method: 'PUT', url: config.domain + 'userSettings/addNew', data: {user: user}});
