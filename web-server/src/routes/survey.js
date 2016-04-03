8778b367e17e3f0841fe300d51019498fba0ecc1
kennedyl <kennedyl@msoe.edu>
2016-04-03 12:12:22 -0600

    mostly working, except there is a broken comparison

--- web-server/src/routes/survey.js
+++ web-server/src/routes/survey.js
@@ -19,30 +19,67 @@ var orgs;
   {
 	  console.log(e);
   });
+function getQbyCat(i, rules, qret,num, res) {
+    
+    
+    // console.log('i: '+i);
+    // console.log('i num: '+rules[i].num);
+    
+    _surveyData.getOrgsMatchingCategory(rules[i].category, function (q) {
+        // console.log("unshuffled");
+        // console.log(q);
+        shuffle(q);
+        // console.log("shuffled!");
+        // console.log(q);
+        
+        
+        for(var j = 0; j< rules[i].num; j++) {
+            qret.push(q[Object.keys(q)[j]]);
+            // console.log(q[Object.keys(q)[j]]);
+        }
+        i++;
+        if(i<rules.length) {
+            getQbyCat(i,rules, qret, num, res);
+        } else {
+           if(qret.length >= num){
+            //    console.log(qret);
+                res.send(qret);
+           } else {
+               console.log("num: "+num);
+               console.log('qret len: '+qret.length);
+               console.log(qret.legnth < num);
+               
+               _surveyData.getAllQuestions(null, function(questionMap){
+                   shuffle(questionMap);
+                   for(var j = 0; qret.legnth < num; j++) {
+                       console.log("pushing");
+                        qret.push(questionMap[Object.keys(questionMap)[j]]);
+                    }
+                    // console.log(qret);
+                   res.send(qret);
+               });
+           }
+           
+        }
+        // console.log(qret);
+        
+            
+        
+    });
+    
+    
 
+}
 router.get('/', function (req, res) {
     var surveySet = jsonfile.readFileSync(surveyFile);
     
-    _surveyData.getOrgsMatchingCategory('test', function (q) {
-        console.log(q);
-    })
+    console.log('num: '+surveySet.num);
     
-    _surveyData.getAllQuestions(null, function(questionMap){
-        if(Object.keys(questionMap).length <= surveySet.num) {
-            res.send(questionMap);
-        } else {
-            var qmap2 =[];
-            for(var i = 0; i< surveySet.num; i++) {
-                qmap2.push(questionMap[Object.keys(questionMap)[i]]);
-            }
-            res.send(qmap2);
-        }
-        
-       
-        
-    }, function(e){
-        console.log("get in get"+e);
-    });
+    qSet = getQbyCat(0, surveySet.rules,[],surveySet.num, res);
+    
+    
+    
+    
 });
 
 router.post("/", function (req, res) {
