16ad58829ee577e79b7eb7542337e0cd956f647b
Ben Beck <ben.beck.764@gmail.com>
2016-05-19 00:43:08 -0500

    fixed error with the wrong modules being opened

--- angular-client/app/directives/editTagModal/editTagModal.controller.js
+++ angular-client/app/directives/editTagModal/editTagModal.controller.js
@@ -1,24 +1,28 @@
-angular.module('editTagModal')
-  .controller('EditTagModalController', ['$modalInstance', 'contents', Controller]);
+(function() {
+  'use strict';
 
-function Controller($modalInstance, contents) {
-  var vm = this;
+  angular.module('editTagModal')
+    .controller('EditTagModalController', ['$modalInstance', 'contents', Controller]);
 
-  vm.tag = contents.tag;
+  function Controller($modalInstance, contents) {
+    var vm = this;
 
-  vm.modifiedTag = {
-    _id: vm.tag._id,
-    tag: vm.tag.text
-  };
-  vm.function = contents.function;
+    vm.tag = contents.tag;
 
-  vm.back = function() {
-    $modalInstance.close('ok');
-  };
+    vm.modifiedTag = {
+      _id: vm.tag._id,
+      tag: vm.tag.text
+    };
+    vm.function = contents.function;
 
-  vm.saveChanges = function() {
-    //editTagModalService.saveChanges(vm.user).then(function(response) {
-    //});
-    $modalInstance.close('ok');
-  };
-}
+    vm.back = function() {
+      $modalInstance.close('ok');
+    };
+
+    vm.saveChanges = function() {
+      //editTagModalService.saveChanges(vm.user).then(function(response) {
+      //});
+      $modalInstance.close('ok');
+    };
+  }
+})();
--- angular-client/app/directives/modal/modal.controller.js
+++ angular-client/app/directives/modal/modal.controller.js
@@ -1,15 +1,18 @@
+(function() {
+  'use strict';
 
-angular.module('modal')
-  .controller('ModalController', ['$modalInstance', 'contents', Controller]);
+  angular.module('modal')
+    .controller('ModalController', ['$modalInstance', 'contents', Controller]);
 
-function Controller($modalInstance, contents) {
-  var vm = this;
+  function Controller($modalInstance, contents) {
+    var vm = this;
 
-  vm.org = contents.org;
-  vm.images = contents.images;
-  
+    vm.org = contents.org;
+    vm.images = contents.images;
 
-  vm.back = function() {
-    $modalInstance.close('ok');
-  };
-}
+
+    vm.back = function() {
+      $modalInstance.close('ok');
+    };
+  }
+})();
