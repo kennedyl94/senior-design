74f155c73f31727dac34668bb1d8964bceb04bfc
Ben Beck <ben.beck.764@gmail.com>
2016-05-19 00:45:53 -0500

    missed one more close

--- angular-client/app/directives/editOrgModal/editOrgModal.controller.js
+++ angular-client/app/directives/editOrgModal/editOrgModal.controller.js
@@ -1,31 +1,33 @@
+(function() {
+  'use strict';
+  angular.module('editOrgModal')
+    .controller('EditOrgModalController', ['$modalInstance', 'contents', Controller]);
 
-angular.module('editOrgModal')
-  .controller('EditOrgModalController', ['$modalInstance', 'contents', Controller]);
+  function Controller($modalInstance, contents) {
+    var vm = this;
 
-function Controller($modalInstance, contents) {
-  var vm = this;
+    vm.org = contents.org;
+  
+    console.log(vm.org);
 
-  vm.org = contents.org;
+    vm.modifiedOrg = {
+      _id: vm.org._id,
+      name: vm.org.name,
+      description: vm.org.description,
+      tags: vm.org.tags,
+      links: vm.org.links,
+      meetings: vm.org.meetings,
+      contact: {
+        name: vm.org.contact.name,
+        email: vm.org.contact.email,
+        phone: vm.org.contact.phone
+      }
+    };
+    vm.function = contents.function;
 
-  console.log(vm.org);
-
-  vm.modifiedOrg = {
-    _id: vm.org._id,
-    name: vm.org.name,
-    description: vm.org.description,
-    tags: vm.org.tags,
-    links: vm.org.links,
-    meetings: vm.org.meetings,
-    contact: {
-      name: vm.org.contact.name,
-      email: vm.org.contact.email,
-      phone: vm.org.contact.phone
-    }
-  };
-  vm.function = contents.function;
-
-  vm.back = function() {
-    console.log(vm.modifiedOrg);
-    $modalInstance.close('ok');
-  };
-}
+    vm.back = function() {
+      console.log(vm.modifiedOrg);
+      $modalInstance.close('ok');
+    };
+  }
+})();
